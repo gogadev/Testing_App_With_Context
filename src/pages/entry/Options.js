53036320bc7a630b860193ca7ccf24fadb7bc8f8
gogadev <radivojevic.g.gordana@gmail.com>
2021-05-13 12:24:27 +0100

    Testing and Coding Alert

--- src/pages/entry/Options.js
+++ src/pages/entry/Options.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import ScoopOptions from "./ScoopOptions";
-import ToppingOptions from "./ToppingOptions"
+import ToppingOptions from "./ToppingOptions";
 import Row from "react-bootstrap/Row";
+import AlertBanner from "../common/AlertBanner";
 
 const Options = ({ optionType }) => {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     // optionType is "scoops" or "toppings"
@@ -13,11 +15,15 @@ const Options = ({ optionType }) => {
       .get(`http://localhost:3030/${optionType}`)
       .then((response) => setItems(response.data))
       .catch((error) => {
-        //error;
+        setError(true);
       });
   }, [optionType]);
 
-  const ItemComponent = optionType === "scoops" ? ScoopOptions : ToppingOptions
+  if (error) {
+    return <AlertBanner />;
+  }
+
+  const ItemComponent = optionType === "scoops" ? ScoopOptions : ToppingOptions;
 
   const optionItems = items.map((item) => (
     <ItemComponent
